31b898864ed247f21b4e74adbc6f6ab58d92a1d7
jakevaughn820 <jakevaughn820@gmail.com>
2022-11-16 13:07:50 -0600

    #003 solved

---
+++ src/#003LengthOfLongestSubstring/lengthOfLongestSubstring.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+	int lengthOfLongestSubstring(string s)
+	{
+		unordered_map<char, int> chars;
+
+		int left = 0;
+		int right = 0;
+
+		int res = 0;
+		while (right < s.length())
+		{
+			char r = s[right];
+			chars[r]++;
+
+			while (chars[r] > 1)
+			{
+				char l = s[left];
+				chars[l]--;
+				left++;
+			}
+
+			res = max(res, right - left + 1);
+
+			right++;
+		}
+
+		return res;
+	}
+};
+
+int main()
+{
+	Solution solution;
+	int sol = solution.lengthOfLongestSubstring("abcabcbb");
+
+	cout << sol;
+	return 0;
+}
